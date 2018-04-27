f9152b256437bd6a604759387b0d36bee4bbdf7e
MiguelMedeiros\Miguel Medeiros <miguel.medeiros@gmail.com>
2018-04-27 10:18:46 -0300

    Removing comments from bat files.
    Removing comments from bat files.

--- mr-watson-dev.bat
+++ mr-watson-dev.bat
@@ -1,6 +1,3 @@
-REM Inicia o Mr. Watson
 start cmd /C "gulp"
 start cmd /C "nodemon start"
-
-REM Inicia o Chrome na pagina do Mr. Watson
 start chrome http://localhost:5000
--- mr-watson.bat
+++ mr-watson.bat
@@ -1,5 +1,2 @@
-REM Inicia o Mr. Watson
 start cmd /C "nodemon start"
-
-REM Inicia o Chrome na pagina do Mr. Watson
 start chrome http://localhost:5000
